7a98542422b4d3a8b2dcf62eef13ba880e73fb0c
gael rose <gael.rose@gmail.com>
2025-09-26 09:26:50 +0000

    ajour champs admin

--- voeux/admin.py
+++ voeux/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Ecole, Formation, Voeu
 
-# Register your models here.
+class EcoleAdmin(admin.ModelAdmin):
+    list_display = ('nom',)  # Affiche le nom dans la liste
+    search_fields = ('nom',) # Barre de recherche sur le nom
+
+class FormationAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'ecole')           # Affiche le nom et l'école associée
+    search_fields = ('nom',)                  # Recherche sur le nom de la formation
+    list_filter = ('ecole',)                  # Filtre par école
+
+class VoeuAdmin(admin.ModelAdmin):
+    list_display = ('user', 'formation', 'date_depot', 'statut')  # Colonnes utiles
+    search_fields = ('user__username', 'formation__nom')           # Recherche par utilisateur ou formation
+    list_filter = ('statut', 'formation__ecole')                   # Filtres par statut et école
+
+admin.site.register(Ecole, EcoleAdmin)
+admin.site.register(Formation, FormationAdmin)
+admin.site.register(Voeu, VoeuAdmin)
